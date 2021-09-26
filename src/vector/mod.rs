f36065a80fafbae884d0f789eab94393fc9d98bc
Igor Couto <igor.fcouto@gmail.com>
2021-09-26 02:21:55 -0300

    Make functions public

--- src/vector/mod.rs
+++ src/vector/mod.rs
@@ -2,45 +2,45 @@ mod test;
 
 use std::ops::{Add, Sub};
 
-struct Vector {
+pub struct Vector {
     x: f32,
     y: f32,
 }
 
 impl Vector {
-    fn new(x: f32, y: f32) -> Vector {
+    pub fn new(x: f32, y: f32) -> Vector {
         Vector { x, y }
     }
 
-    fn origin() -> Vector {
+    pub fn origin() -> Vector {
         Vector { x: 0f32, y: 0f32 }
     }
 
-    fn print(&self) {
+    pub fn print(&self) {
         println!("Vector: {}, {}", self.x, self.y);
     }
 
-    fn add_values(&mut self, x: f32, y: f32) {
+    pub fn add_values(&mut self, x: f32, y: f32) {
         self.x += x;
         self.y += y;
     }
 
-    fn sub_values(&mut self, x: f32, y: f32) {
+    pub fn sub_values(&mut self, x: f32, y: f32) {
         self.x -= x;
         self.y -= y;
     }
 
-    fn add_vector(&mut self, other: &Vector) {
+    pub fn add_vector(&mut self, other: &Vector) {
         self.x += other.x;
         self.y += other.y;
     }
 
-    fn sub_vector(&mut self, other: &Vector) {
+    pub fn sub_vector(&mut self, other: &Vector) {
         self.x -= other.x;
         self.y -= other.y;
     }
 
-    fn mag(&self) -> f32 {
+    pub fn mag(&self) -> f32 {
         (self.x.powf(2f32) + self.y.powf(2f32)).sqrt()
     }
 }
